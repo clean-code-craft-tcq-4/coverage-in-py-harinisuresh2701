de15e95bc05b73452ee9d935f3e7b61af2dea516
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-07-18 14:56:27 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -6,7 +6,12 @@ class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     cooling_type_object = typewise_alert.cooling_type(100, 50, 20)
     self.assertTrue(cooling_type_object.breach_type == 'TOO_HIGH')
-
+    
+    cooling_type_object = typewise_alert.cooling_type(5, 50, 20)
+    self.assertTrue(cooling_type_object.breach_type == 'TOO_LOW')
+        
+    cooling_type_object = typewise_alert.cooling_type(30, 50, 20)
+    self.assertTrue(cooling_type_object.breach_type == 'NORMAL')
 
 if __name__ == '__main__':
   unittest.main()

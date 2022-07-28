bc760775ac0fa31f6d8cadc1ff4cd95b02107092
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-07-28 07:59:16 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -2,10 +2,17 @@ import unittest
 import typewise_alert
 
 recepient = "a.b@c.com"
-
-def print_message_on_console_stub(message):
-    print(message)  
+header = 0xfeed
     
+def print_message_on_console_stub(message):
+    print(message) 
+    
+def check_and_alert_stub(coolingType, temperatureInC, classify_temperature_breach, send_to_controller_or_email, print_message_on_console):
+  breachType =\
+    classify_temperature_breach(coolingType, temperatureInC)
+  output = send_to_controller_or_email(breachType,print_message_on_console)
+  return output
+
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     cooling_type_object = typewise_alert.cooling_type(100, 50, 20)
@@ -15,6 +22,12 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(cooling_type_object.breach_type == 'TOO_LOW')
         
     cooling_type_object = typewise_alert.cooling_type(30, 50, 20)
+    self.assertTrue(cooling_type_object.breach_type == 'NORMAL')
+    
+    cooling_type_object = typewise_alert.cooling_type(50, 50, 20)
+    self.assertTrue(cooling_type_object.breach_type == 'NORMAL')
+    
+    cooling_type_object = typewise_alert.cooling_type(20, 50, 20)
     self.assertTrue(cooling_type_object.breach_type == 'NORMAL')
     
   def test_classify_temperature_breach(self):
@@ -32,19 +45,19 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.send_to_controller('TOO_LOW',print_message_on_console_stub) == "0xfeed, TOO_LOW")
   
   def test_send_to_email(self):
-    recepient_message,breach_message_on_console = typewise_alert.send_to_email('TOO_HIGH', print_message_on_console_stub)
+    recepient_message,breach_message_on_console = typewise_alert.send_to_email('TOO_HIGH', print_message_on_console_stub,recepient)
     self.assertTrue(recepient_message == "To: a.b@c.com")
     self.assertTrue(breach_message_on_console == "Hi, the temperature is too high")
     
-    recepient_message,breach_message_on_console = typewise_alert.send_to_email('TOO_LOW', print_message_on_console_stub)
+    recepient_message,breach_message_on_console = typewise_alert.send_to_email('TOO_LOW', print_message_on_console_stub,recepient)
     self.assertTrue(breach_message_on_console == "Hi, the temperature is too low")
     
   def test_check_and_alert(self):
-    breachType, controller_message = typewise_alert.check_and_alert('TO_CONTROLLER', "PASSIVE_COOLING", 45, classify_temperature_breach_stub, send_to_controller_stub, send_to_email_stub)
+    breachType, controller_message = check_and_alert_stub("PASSIVE_COOLING", 45, typewise_alert.classify_temperature_breach, typewise_alert.send_to_controller)
     self.assertTrue(breachType == "TOO_HIGH")
     self.assertTrue(controller_message == "0xfeed, TOO_HIGH")
     
-    breachType, controller_message = typewise_alert.check_and_alert('TO_EMAIL', "PASSIVE_COOLING", -5, classify_temperature_breach_stub, send_to_controller_stub, send_to_email_stub)    
+    breachType, controller_message = typewise_alert.check_and_alert("PASSIVE_COOLING", -5, typewise_alert.classify_temperature_breach, typewise_alert.send_to_email)    
     self.assertTrue(breachType == "TOO_LOW")
     self.assertTrue(controller_message[0] == "To: a.b@c.com")
     self.assertTrue(controller_message[1] == "Hi, the temperature is too low")
